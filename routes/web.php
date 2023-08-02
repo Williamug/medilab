3a166a27200b9b9774794aca0c0af6728335085e
Asaba William <asabawilliamdk@yahoo.com>
2023-08-02 11:24:33 +0300

    Add category routes and clean ups

--- routes/web.php
+++ routes/web.php
@@ -3,27 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DataFeedController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CatagoriesController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::redirect('/', 'login');
 
-Route::middleware(['auth:sanctum', 'verified'])->group(function () {
-
-    // Route for the getting the data feed
-    Route::get('/json-data-feed', [DataFeedController::class, 'getDataFeed'])->name('json_data_feed');
-
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-    Route::fallback(function() {
-        return view('pages/utility/404');
-    });    
+Route::controller(CatagoriesController::class)->group(function () {
+    Route::get('/catagories', 'index')->name('catagories.index');
+    Route::post('/catagories', 'store')->name('catagories.store');
+    Route::patch('/catagories/{catagory}', 'update')->name('catagories.update');
 });
