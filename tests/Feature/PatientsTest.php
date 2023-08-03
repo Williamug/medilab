87195ef3cb991d30108221d08500e84a67e0df02
Asaba William <asabawilliamdk@yahoo.com>
2023-08-03 08:14:19 +0300

    Update test case

--- tests/Feature/PatientsTest.php
+++ tests/Feature/PatientsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Patient;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -24,8 +25,97 @@ class PatientsTest extends TestCase
     }
 
     /** @test */
-    public function test_that_create_page_returns_a_success_response(): void
+    public function test_that_a_user_can_add_a_patients_record(): void
     {
-        // 
+        // $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->post(route('patients.store'), [
+            'full_name' => 'Allan Muntu',
+            'phone_number' => '0700000123',
+            'gender' => 'male',
+            'birth_date' => '1/1/1990',
+            'residence' => 'kyanja',
+        ]);
+
+        $this->assertDatabaseCount('patients', 1);
+        $response->assertRedirect(route('patients.index'));
+    }
+
+    /** @test */
+    public function full_name_is_required(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post(route('patients.store'), [
+            'full_name' => '',
+            'phone_number' => '0700000123',
+            'gender' => 'male',
+            'birth_date' => '1/1/1990',
+            'residence' => 'kyanja',
+        ]);
+
+        $response->assertSessionHasErrors('full_name');
+    }
+
+    /** @test */
+    public function gender_is_required(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->post(route('patients.store'), [
+            'full_name' => 'Allan Muntu',
+            'phone_number' => '0700000123',
+            'gender' => '',
+            'birth_date' => '1/1/1990',
+            'residence' => 'kyanja',
+        ]);
+
+        $response->assertSessionHasErrors('gender');
+    }
+
+    /** @test */
+    public function patient_info_can_be_updated(): void
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user)->post(route('patients.store'), [
+            'full_name' => 'Allan Muntu',
+            'phone_number' => '0700000123',
+            'gender' => 'male',
+            'birth_date' => '1/1/1990',
+            'resident' => 'Kyanja',
+        ]);
+
+        $patient = Patient::first();
+
+        $response = $this->actingAs($user)->put(route('patients.update', $patient->id), [
+            'full_name' => 'Allan Muntu Junior',
+            'phone_number' => '',
+            'gender' => 'male',
+            'birth_date' => '1/1/1990',
+            'residence' => 'kasangati',
+        ]);
+
+        $this->assertEquals('Allan Muntu Junior', Patient::first()->full_name);
+        $this->assertEquals('kasangati', Patient::first()->residence);
+        $response->assertRedirect(route('patients.update', $patient->id));
+    }
+
+    /** @test */
+    public function patient_can_be_deleted(): void
+    {
+        $this->withoutExceptionHandling();
+        $user = User::factory()->create();
+
+        $this->actingAs($user)->post(route('patients.store'), [
+            'full_name' => 'Allan Muntu',
+            'phone_number' => '0700000123',
+            'gender' => 'male',
+            'birth_date' => '1/1/1990',
+            'resident' => 'Kyanja',
+        ]);
+        $patient = Patient::first();
+        $response = $this->delete(route('patients.destroy', $patient->id));
+
+        $this->assertDatabaseCount('patients', 0);
+        $response->assertRedirect(route('patients.index'));
     }
 }
