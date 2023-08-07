772d97d8d78d52f9c40f7c3730c5acf1d13b83b8
Asaba William <asabawilliamdk@yahoo.com>
2023-08-07 05:05:24 +0300

    Add static method

--- app/Models/TestRequst.php
+++ app/Models/TestRequst.php
@@ -10,4 +10,12 @@ class TestRequst extends Model
     use HasFactory;
 
     protected $fillable = ['patient_id', 'test_service_id', 'spacemen_id', 'result_id'];
+
+    public static function search($query)
+    {
+        // filter results based on test name
+        return empty($query)
+            ? static::query()
+            : static::where('patient_id', 'like', '%' . $query . '%');
+    }
 }
