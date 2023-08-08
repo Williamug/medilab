4c66e62c6737f0e1369c3b51eebae721b6d78aaf
Asaba William <asabawilliamdk@yahoo.com>
2023-08-08 12:09:20 +0300

    Add `Admin` role

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -14,6 +14,7 @@ class RolesSeeder extends Seeder
     public function run(): void
     {
         $roles = [
+            'Admin',
             'Reception',
             'Accountant',
             'Lab Attendant',
