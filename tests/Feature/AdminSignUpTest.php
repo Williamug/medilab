d42ec305b9ef2e9b93739a13d5e75ca9f86ad998
Asaba William <asabawilliamdk@yahoo.com>
2023-08-08 12:10:33 +0300

    Update test

--- tests/Feature/AdminSignUpTest.php
+++ tests/Feature/AdminSignUpTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class AdminSignUpTest extends TestCase
@@ -14,5 +16,22 @@ class AdminSignUpTest extends TestCase
     public function admin_signup_page_can_render_with_a_success_response(): void
     {
         $reponse = $this->get(route('admin-signup.create'));
+
+        $reponse->assertStatus(200);
+    }
+
+    /** @test */
+    public function create_an_admin_account_successfully(): void
+    {
+        $this->markTestSkipped();
+        $response = $this->post(route('admin-singup.store'), [
+            'name' => 'Allan Muntu',
+            'email' => 'muntuallan@gmail.com',
+            'password' => Hash::make('321wordpass'), //password123
+        ]);
+
+        $this->assertDatabaseCount('users', 1);
+        $response->assertRedirect(route('login'));
+        $response->assertStatus(302);
     }
 }
