1afa3106642a35ec5db24e9cc8884e21859acadc
Asaba William <asabawilliamdk@yahoo.com>
2023-08-08 12:09:48 +0300

    Update blade files

--- resources/views/pages/roles-permissions/assign-roles/show.blade.php
+++ resources/views/pages/roles-permissions/assign-roles/show.blade.php
@@ -1,40 +1,41 @@
-<x-master>
+<x-app-layout>
     <x-slot:title>
-        <div class="flex">
-            <div class="flex-1">
-                Roles
-            </div>
-            <div class="flex">
-                <a href="{{ route('roles.index') }}"
-                    class="flex px-2 mb-2 ml-4 text-white bg-gray-700 rounded hover:bg-gray-500">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-caret-left w-6 h-6 pt-2"
-                        viewBox="0 0 16 16">
-                        <path
-                            d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z" />
-                    </svg>
-                    Back
-                </a>
 
-                @can('Edit student')
-                    <a href="{{ route('roles.edit', $user) }}"
-                        class="flex px-2 mb-2 ml-4 text-white bg-green-300 rounded hover:bg-green-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 pt-2 bi bi-pencil-square"
+    </x-slot:title>
+    <x-app.card>
+        <x-slot name="banner">
+            <div class="flex">
+                <div class="flex-1">
+                    Roles
+                </div>
+                <div class="flex">
+                    <a href="{{ route('roles.index') }}"
+                        class="flex px-2 mb-2 ml-4 text-white bg-gray-700 rounded hover:bg-gray-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-6 h-6 pt-2 bi bi-caret-left"
                             viewBox="0 0 16 16">
                             <path
-                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                            <path fill-rule="evenodd"
-                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z" />
                         </svg>
-                        Edit
+                        Back
                     </a>
-                @endcan
+
+                    @can('Edit student')
+                        <a href="{{ route('roles.edit', $user) }}"
+                            class="flex px-2 mb-2 ml-4 text-white bg-green-300 rounded hover:bg-green-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                class="w-6 h-6 pt-2 bi bi-pencil-square" viewBox="0 0 16 16">
+                                <path
+                                    d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456l-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                <path fill-rule="evenodd"
+                                    d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                            </svg>
+                            Edit
+                        </a>
+                    @endcan
+                </div>
             </div>
-        </div>
-    </x-slot:title>
-    <x-card>
-        <x-slot name="banner">
         </x-slot>
-        <x-flash-message />
+        <x-app.flash-message />
 
         <div>
             <div class="flex mt-10 border-t">
@@ -53,7 +54,7 @@
                 <div class="px-4 py-2 text-base font-bold">Role:</div>
                 <div class="px-4 py-2">
                     @foreach ($user->getRoleNames() as $role)
-                        <span class="text-sm px-2 bg-gray-500 text-white rounded">{{ $role }}</span>
+                        <span class="px-2 text-sm text-white bg-gray-500 rounded">{{ $role }}</span>
                     @endforeach
                 </div>
             </div>
@@ -63,7 +64,7 @@
                 <div class="px-4 py-2">
                     <ul class="grid grid-cols-3 gap-4">
                         @foreach ($user->getPermissionsViaRoles() as $permission)
-                            <li class="bg-gray-500 mr-2 px-2 rounded mb-1 text-white text-sm">
+                            <li class="px-2 mb-1 mr-2 text-sm text-white bg-gray-500 rounded">
                                 {{ $permission->name }}
                             </li>
                         @endforeach
@@ -72,5 +73,5 @@
             </div>
         </div>
         </div>
-    </x-card>
-</x-master>
+    </x-app.card>
+</x-app-layout>
--- resources/views/pages/roles-permissions/give-permissions/edit.blade.php
+++ resources/views/pages/roles-permissions/give-permissions/edit.blade.php
@@ -1,15 +1,16 @@
-<x-master>
+<x-app-layout>
     <x-slot:title>
         Roles
     </x-slot:title>
-    <x-card>
+    <x-app.card>
         <x-slot name="banner">
+            Roles
         </x-slot>
         <div>
             <div>
                 @if ($errors->any())
                     <div class="p-4 bg-red-200 border border-red-400 rounded">
-                        <x-validation-errors class="mb-4" />
+                        <x-jet-validation-errors class="mb-4" />
                     </div>
                 @endif
 
@@ -25,18 +26,18 @@
                     <div class="mt-8">
                         <!-- role-->
                         <div class="mt-4">
-                            <x-label for="role_id" value="{{ __('Role') }}" />
+                            <x-jet-label for="role_id" value="{{ __('Role') }}" />
                             <select id="role_id" name="role_id" wire:model="roles"
                                 class="block w-1/2 mt-1 border-gray-300 rounded-md shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-900 dark:bg-gray-700 dark:text-gray-400">
                                 <option value="{{ $role->id }}">{{ $role->name }}
                                 </option>
                             </select>
-                            <x-input-error for="role_id" />
+                            <x-jet-input-error for="role_id" />
                         </div>
                         <!-- /.role-->
-                        <div class="border border-t my-4 mb-12"></div>
+                        <div class="my-4 mb-12 border border-t"></div>
 
-                        <div class="text-base mb-4 text-center border border-red-200 bg-red-50 rounded px-2 py-1">
+                        <div class="px-2 py-1 mb-4 text-base text-center border border-red-200 rounded bg-red-50">
                             <span class="text-red-500">Note:</span> Please check all the permissions you want to assign
                             to this
                             role.
@@ -45,32 +46,26 @@
                             be granted to this role.
                         </div>
 
-                        {{-- <div class="flex mb-3">
-                <x-checkbox id="checked" class="mr-2 mt-1" wire:model="SelectedPermissions" />
-                <x-label for="checked" value="Give Full Permissions" />
-                <div></div>
-            </div> --}}
-
                         <div class="grid grid-cols-3 gap-4">
                             @foreach ($modules as $module)
-                                <div class="border p-4 rounded-md shadow-md bg-gray-50">
-                                    <div class="font-bold mb-4">
+                                <div class="p-4 border rounded-md shadow-md bg-gray-50">
+                                    <div class="mb-4 font-bold">
                                         {{ $module->name }}
                                     </div>
                                     <div>
                                         <ul class="list-none">
                                             @foreach ($module->permissions as $permission)
-                                                <div class="flex my-4 pl-2">
+                                                <div class="flex pl-2 my-4">
                                                     @if (in_array($permission->id, $role_permissions))
-                                                        <x-checkbox id="{{ $permission->id }}" class="mr-2 mt-1"
+                                                        <x-jet-checkbox id="{{ $permission->id }}" class="mt-1 mr-2"
                                                             name="permissions[]" value="{{ $permission->id }}"
                                                             checked />
                                                     @else
-                                                        <x-checkbox id="{{ $permission->id }}" class="mr-2 mt-1"
+                                                        <x-jet-checkbox id="{{ $permission->id }}" class="mt-1 mr-2"
                                                             name="permissions[]" value="{{ $permission->id }}" />
                                                     @endif
                                                     <li>
-                                                        <x-label for="{{ $permission->id }}"
+                                                        <x-jet-label for="{{ $permission->id }}"
                                                             value="{{ $permission->name }}" />
                                                     </li>
                                                 </div>
@@ -82,13 +77,13 @@
                         </div>
                     </div>
                     <div>
-                        <x-button class="mt-4">
+                        <x-jet-button class="mt-4">
                             {{ __('Update Permissions') }}
-                        </x-button>
+                        </x-jet-button>
                     </div>
                 </form>
             </div>
 
         </div>
-    </x-card>
-</x-master>
+    </x-app.card>
+</x-app-layout>
