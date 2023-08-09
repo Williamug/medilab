e1fbb3666912532aa8eb3b276b5ca8c8638954e6
Asaba William <asabawilliamdk@yahoo.com>
2023-08-09 09:33:44 +0300

    Add accounting resources

---
+++ resources/views/livewire/accounting-list-component.blade.php
@@ -0,0 +1,148 @@
+<div>
+    <div class="py-8">
+        <div class="flex flex-col my-2 sm:flex-row">
+            <div class="flex flex-row mb-1 sm:mb-0">
+                <div class="relative">
+                    <select
+                        class="block w-full h-full px-4 py-2 pr-8 leading-tight text-gray-700 bg-white border border-gray-400 rounded-l appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
+                        wire:model="perPage">
+                        <option>3</option>
+                        <option>6</option>
+                        <option>12</option>
+                    </select>
+                    <div class="absolute inset-y-0 right-0 flex items-center px-2 text-gray-700 pointer-events-none">
+
+                    </div>
+                </div>
+            </div>
+            <div class="relative block">
+                <span class="absolute inset-y-0 left-0 flex items-center h-full pl-2">
+                    <svg viewBox="0 0 24 24" class="w-4 h-4 text-gray-500 fill-current">
+                        <path
+                            d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
+                        </path>
+                    </svg>
+                </span>
+                <input wire:model="search" placeholder="Search"
+                    class="block w-full py-2 pl-8 pr-6 text-sm text-gray-700 placeholder-gray-400 bg-white border border-b border-gray-400 rounded-l rounded-r appearance-none sm:rounded-l-none focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+            </div>
+        </div>
+        <div class="px-4 py-4 -mx-4 overflow-x-auto sm:-mx-8 sm:px-8">
+            <div class="inline-block min-w-full overflow-hidden rounded shadow">
+                <table class="min-w-full leading-normal">
+                    <thead>
+                        <tr>
+                            <th
+                                class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                                Patient
+                            </th>
+
+                            <th
+                                class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                                Test done
+                            </th>
+
+                            <th
+                                class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                                Price
+                            </th>
+
+                            @can('view spacemen')
+                                <th
+                                    class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                                    Actions
+                                </th>
+                            @endcan
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($accounts as $account)
+                            <tr>
+                                <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ $account->patient->full_name }}
+                                    </p>
+                                </td>
+
+                                <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ $account->test_service->catagory->catagory_name }}
+                                    </p>
+                                </td>
+
+                                <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                    <p class="text-gray-900 whitespace-no-wrap">
+                                        {{ $account->test_service->price }}
+                                    </p>
+                                </td>
+
+
+
+                                @can('view accounting')
+                                    <td class="px-5 py-5 text-sm bg-white border-b border-gray-200">
+                                        <p class="text-gray-900 whitespace-no-wrap">
+                                        <div class="flex">
+                                            <div class="flex">
+                                                <a href="{{ route('accountings.show', $account) }}"
+                                                    class="text-green-700 hover:underline hover:text-green-900"
+                                                    title="View">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        class="w-4 h-4 mr-3 text-blue-500 hover:text-blue-800 bi bi-layout-text-window-reverse"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M13 6.5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5zm0 3a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 .5-.5zm-.5 2.5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1 0-1h5z" />
+                                                        <path
+                                                            d="M14 0a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12zM2 1a1 1 0 0 0-1 1v1h14V2a1 1 0 0 0-1-1H2zM1 4v10a1 1 0 0 0 1 1h2V4H1zm4 0v11h9a1 1 0 0 0 1-1V4H5z" />
+                                                    </svg>
+                                                </a>
+                                            @endcan
+
+                                            {{-- <a href="{{ route('spacemens.edit', $spacemen) }}"
+                                                    class="text-blue-700 hover:underline hover:text-blue-900"
+                                                    title="Edit">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                        class="w-4 h-4 mr-3 text-green-600 bi bi-pencil-square hover:text-green-800"
+                                                        viewBox="0 0 16 16">
+                                                        <path
+                                                            d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                                        <path fill-rule="evenodd"
+                                                            d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                                    </svg>
+                                                </a>
+                                            </div>
+                                            <div>
+                                                <form action="{{ route('spacemens.destroy', $spacemen) }}" method="POST">
+                                                    @csrf
+                                                    @method('DELETE')
+
+                                                    <button
+                                                        class="text-red-700 hover:underline hover:text-red-900 focus:outline-none dark:text-red-800 dark:hover:text-red-900"
+                                                        onclick="javascript:return confirm('You are about to delete this. Are you sure you want to continue?')">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                                                            class="w-4 h-4 mr-3 text-red-600 bi bi-trash hover:text-red-800"
+                                                            viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                                                            <path fill-rule="evenodd"
+                                                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
+                                                        </svg>
+                                                    </button>
+                                                </form>
+                                            </div> --}}
+                                        </div>
+                                        </p>
+                                </td>
+                                {{-- @endcan --}}
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <div class="flex flex-col items-center px-5 py-5 bg-white border-t xs:flex-row xs:justify-between ">
+                    <div class="inline-flex mt-2 xs:mt-0">
+                        {{-- {{ $accounts->links() }} --}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
