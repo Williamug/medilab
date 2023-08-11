6feadc78946e97b39c51841f8aced5595e8f05fa
Asaba William <asabawilliamdk@yahoo.com>
2023-08-11 14:37:40 +0300

    fix the width

--- resources/views/components/app/text.blade.php
+++ resources/views/components/app/text.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'form-input rounded-md shodow-sm', 'rows' => 6]) !!} rows="6"> {{ $slot }}</textarea>
+<textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'form-input rounded-md shodow-sm w-full', 'rows' => 6]) !!} rows="6"> {{ $slot }}</textarea>
