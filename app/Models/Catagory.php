d565dd88e70af7a448a8597c71b9310598c60279
Asaba William <asabawilliamdk@yahoo.com>
2023-08-11 15:13:49 +0300

    Add `SoftDeletes` trait

--- app/Models/Catagory.php
+++ app/Models/Catagory.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Catagory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
 
     protected $fillable = ['user_id', 'catagory_name', 'description'];
 
