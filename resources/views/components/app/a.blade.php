226b043a8e5bb80ea09491733d8ca1cf0bbc04ff
Asaba William <asabawilliamdk@yahoo.com>
2023-08-11 14:37:22 +0300

    update

--- resources/views/components/app/a.blade.php
+++ resources/views/components/app/a.blade.php
@@ -1,4 +1,4 @@
 <a
-    {{ $attributes->merge(['class' => 'inline-flex items-center px-6 py-2 ml-4 bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:shadow-outline-indigo disabled:opacity-25 transition ease-in-out duration-150', 'href' => '']) }}>
+    {{ $attributes->merge(['class' => 'inline-flex items-center px-1 py-1 mb-2 bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:border-indigo-900 focus:shadow-outline-indigo disabled:opacity-25 transition ease-in-out duration-150', 'href' => '']) }}>
     {{ $slot }}
 </a>
