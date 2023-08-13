4226cbbe3578365c8c79da43d8c4c35315de50a1
Asaba William <asabawilliamdk@yahoo.com>
2023-08-14 00:09:06 +0300

    Update migrations

--- database/migrations/2023_08_02_234425_create_patients_table.php
+++ database/migrations/2023_08_02_234425_create_patients_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('patients', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable();
+            $table->foreignId('next_of_kin_id')->nullable();
+            $table->string('registration_number');
             $table->string('full_name');
             $table->string('gender');
-            $table->integer('age')->nullable();
+            $table->unsignedInteger('age')->nullable();
             $table->date('date_of_birth')->nullable();
             $table->string('phone_number')->nullable();
             $table->text('email')->nullable();
