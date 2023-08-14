d3ad8db6d07254f9227e8bb8dae9e4904af70a5b
Asaba William <asabawilliamdk@yahoo.com>
2023-08-14 10:11:36 +0300

    Add patient and lab_service relationship

--- app/Models/TestResult.php
+++ app/Models/TestResult.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TestResult extends Model
@@ -11,4 +12,14 @@ class TestResult extends Model
     use HasFactory, SoftDeletes;
 
     protected $fillable = ['user_id', 'patient_id', 'lab_service_id', 'spacemen_id', 'result_option_id', 'test_identity', 'comment'];
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
+    }
+
+    public function lab_service(): BelongsTo
+    {
+        return $this->belongsTo(LabServices::class);
+    }
 }
