5c486692000b9fa4314da9a5f8612d509c8de40c
Asaba William <asabawilliamdk@yahoo.com>
2023-08-24 22:11:29 +0300

    Update model

--- app/Models/LabService.php
+++ app/Models/LabService.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LabService extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['service_name', 'user_id'];
+
+    public static function search($query)
+    {
+        // filter results
+        return empty($query)
+            ? static::query()
+            : static::where('service_name', 'like', '%' . $query . '%');
+    }
 }
