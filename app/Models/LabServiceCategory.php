19549d54424e6c692d6d5d778bedf93100d3c4ae
Asaba William <asabawilliamdk@yahoo.com>
2023-08-24 22:11:40 +0300

    Update model

--- app/Models/LabServiceCategory.php
+++ app/Models/LabServiceCategory.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LabServiceCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['category_name', 'user_id'];
+
+    protected $casts = ['created_at' => 'datetime'];
+
+    public static function search($query)
+    {
+        // filter results
+        return empty($query)
+            ? static::query()
+            : static::where('category_name', 'like', '%' . $query . '%');
+    }
 }
