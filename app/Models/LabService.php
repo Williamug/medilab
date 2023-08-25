4ae3535a727135bfd831b5c5e7a0f1435fe90420
Asaba William <asabawilliamdk@yahoo.com>
2023-08-25 07:30:18 +0300

    Update model

--- app/Models/LabService.php
+++ app/Models/LabService.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LabService extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['service_name', 'user_id'];
+    protected $fillable = ['service_name', 'user_id', 'price', 'lab_service_category_id', 'result_id'];
+
+    protected $casts = ['created_at' => 'datetime'];
 
     public static function search($query)
     {
@@ -18,5 +22,15 @@ class LabService extends Model
         return empty($query)
             ? static::query()
             : static::where('service_name', 'like', '%' . $query . '%');
+    }
+
+    public function result_options(): BelongsToMany
+    {
+        return $this->belongsToMany(ResultOption::class);
+    }
+
+    public function lab_service_category(): BelongsTo
+    {
+        return $this->belongsTo(LabServiceCategory::class);
     }
 }
