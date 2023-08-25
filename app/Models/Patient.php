c2cd0975c1650881a2627b8478704b1691409cd1
Asaba William <asabawilliamdk@yahoo.com>
2023-08-25 22:57:53 +0300

    compute age

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Attribute;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -13,6 +15,13 @@ class Patient extends Model
 
     protected $guarded = [];
 
+    protected $with = ['patient_visits'];
+
+    //Accessor for Age.
+    public function ageFromDob()
+    {
+        return Carbon::parse($this->attributes['date_of_birth'])->age;
+    }
 
     public static function search($query)
     {
@@ -25,5 +34,10 @@ class Patient extends Model
     public function patient_visits(): HasMany
     {
         return $this->hasMany(PatientVisit::class);
+    }
+
+    public function test_orders(): HasMany
+    {
+        return $this->hasMany(TestOrder::class);
     }
 }
