c14119b32e99d172d8381037cca6e30f0e71dd56
Asaba William <asabawilliamdk@yahoo.com>
2023-08-25 11:01:45 +0300

    wip

--- app/Models/PatientVisit.php
+++ app/Models/PatientVisit.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PatientVisit extends Model
@@ -18,5 +19,10 @@ class PatientVisit extends Model
         return empty($query)
             ? static::query()
             : static::where('next_of_kin_name', 'like', '%' . $query . '%');
+    }
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
     }
 }
