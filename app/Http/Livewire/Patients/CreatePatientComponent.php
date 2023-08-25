49fe8282169ccbd1effc1cbf856f49c5d586807c
Asaba William <asabawilliamdk@yahoo.com>
2023-08-25 22:56:57 +0300

    Add patient, patient visit and test order

--- app/Http/Livewire/Patients/CreatePatientComponent.php
+++ app/Http/Livewire/Patients/CreatePatientComponent.php
@@ -6,8 +6,11 @@ use App\Models\LabService;
 use App\Models\Patient;
 use App\Models\PatientVisit;
 use App\Models\TestOrder;
+use App\Rules\LabServiceRule;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
+use Illuminate\Support\Arr;
 
 class CreatePatientComponent extends Component
 {
@@ -15,9 +18,12 @@ class CreatePatientComponent extends Component
 
     // patient
     public $full_name;
+
     public $gender;
-    public $birth_date = 'dob';
-    public $age;
+    public $weeks;
+    public $months;
+    public $years;
+
     public $date_of_birth;
     public $phone_number;
     public $email;
@@ -35,31 +41,18 @@ class CreatePatientComponent extends Component
     // public $kin_email;
     public $kin_residence;
 
+    public $selectedBirthDate = NULL;
+    public $selectedPeriod = NULL;
+
     public $lab_service_id;
+
+    // collection
+    public $birth_dates;
+    public $periods;
 
     public $inputs = [];
     public $i      = 1;
 
-    protected $rules = [
-        // patient
-        'full_name'     => 'required|min:3|string',
-        'gender'        => 'required|min:3|string',
-        'date_of_birth' => 'sometimes|nullable|date',
-        'phone_number'  => 'numeric|min:10',
-        'email'         => 'sometimes|nullable|email',
-        'residence'     => 'string|max:255',
-
-        // patient visit
-        'age'              => 'sometimes|nullable|numeric',
-        'temperature'      => 'sometimes|nullable|numeric',
-        'weight'           => 'sometimes|nullable|numeric',
-        'height'           => 'sometimes|nullable|numeric',
-        'kin_full_name'    => 'sometimes|nullable|min:3|string',
-        'kin_gender'       => 'sometimes|nullable|min:3|string',
-        'kin_phone_number' => 'sometimes|nullable|numeric|min:10',
-        'kin_residence'    => 'sometimes|nullable|string|max:255',
-        // 'kin_email' => 'sometimes|nullable|email',
-    ];
 
     public function add($i)
     {
@@ -75,6 +68,10 @@ class CreatePatientComponent extends Component
 
     public function mount()
     {
+        $this->birth_dates =  collect(['Date of birth', 'Age']);
+
+        $this->periods = collect(['weeks', 'months', 'years']);
+
         $this->lab_services = LabService::all();
     }
 
@@ -96,42 +93,81 @@ class CreatePatientComponent extends Component
 
     public function storePatient()
     {
-        $this->validate();
+        // dd($this->lab_service_id);
+        // dd(in_array(2, $this->lab_service_id));
+        $this->validate([
+            // patient
+            // 'lab_service_id' =>
+            // [
+            //     function ($attribute, $value, $fail) {
+            //         if (!in_array($value, $this->lab_service_id)) {
+            //             $fail('Lab service must be unique.');
+            //         }
+            //     },
+            // ],
+            'selectedBirthDate' => 'required',
+            'full_name'         => 'required|min:3|string',
+            'gender'            => 'required|min:3|string',
+            'date_of_birth'     => 'sometimes|nullable|date',
+            'phone_number'      => 'numeric|min:10',
+            'email'             => 'sometimes|nullable|email',
+            'residence'         => 'string|max:255',
 
-        $patient = Patient::create([
-            'user_id'        => auth()->id(),
-            'patient_number' => $this->patient_number(),
-            'full_name'      => $this->full_name,
-            'gender'         => $this->gender,
-            'date_of_birth'  => $this->date_of_birth,
-            'age'            => $this->age,
-            'phone_number'   => $this->phone_number,
-            'email'          => $this->email,
-            'residence'      => $this->residence,
-        ]);
+            // patient visit
+            'temperature'      => 'sometimes|nullable|numeric',
+            'weight'           => 'sometimes|nullable|numeric',
+            'height'           => 'sometimes|nullable|numeric',
+            'kin_full_name'    => 'sometimes|nullable|min:3|string',
+            'kin_gender'       => 'sometimes|nullable|min:3|string',
+            'kin_phone_number' => 'sometimes|nullable|numeric|min:10',
+            'kin_residence'    => 'sometimes|nullable|string|max:255',
+        ], ['selectedBirthDate.required' => 'This field can not be empty. Select date of birth or age']);
 
-        PatientVisit::create([
-            'user_id'          => auth()->id(),
-            'patient_id'       => $patient->id,
-            'patient_age'      => $this->age,
-            'temperature'      => $this->temperature,
-            'weight'           => $this->weight,
-            'height'           => $this->height,
-            'kin_full_name'    => $this->kin_full_name,
-            'kin_gender'       => $this->kin_gender,
-            'kin_phone_number' => $this->kin_phone_number,
-            'kin_residence'    => $this->kin_residence,
-        ]);
+        DB::transaction(function () {
 
-        foreach ($this->lab_service_id as $key => $value) {
-            TestOrder::create([
+            $patient = Patient::create([
                 'user_id'        => auth()->id(),
-                'patient_id'     => $patient->id,
-                'lab_service_id' => $this->lab_service_id[$key],
-                'order_number'   => $this->order_number()
+                'patient_number' => $this->patient_number(),
+                'full_name'      => $this->full_name,
+                'gender'         => $this->gender,
+                'phone_number'   => $this->phone_number,
+                'email'          => $this->email,
+                'residence'      => $this->residence,
             ]);
-        }
 
+            $patient_visit = PatientVisit::create([
+                'user_id'          => auth()->id(),
+                'patient_id'       => $patient->id,
+                // 'patient_age'      => $this->age,
+                'temperature'      => $this->temperature,
+                'weight'           => $this->weight,
+                'height'           => $this->height,
+                'kin_full_name'    => $this->kin_full_name,
+                'kin_gender'       => $this->kin_gender,
+                'kin_phone_number' => $this->kin_phone_number,
+                'patient_relation' => $this->patient_relation,
+                'kin_residence'    => $this->kin_residence,
+            ]);
+
+            foreach ($this->lab_service_id as $key => $value) {
+                TestOrder::create([
+                    'user_id'        => auth()->id(),
+                    'patient_id'     => $patient->id,
+                    'lab_service_id' => $this->lab_service_id[$key],
+                    'order_number'   => $this->order_number()
+                ]);
+
+                // if ($this->date_of_birth !== null && $this->age === null) {
+                //     $patient->update([
+                //         'date_of_birth' => $this->date_of_birth,
+                //     ]);
+                // } elseif ($this->date_of_birth === null && $this->age !== null) {
+                //     $patient_visit->update([
+                //         'patient_age' => $this->age,
+                //     ]);
+                // }
+            }
+        });
         return redirect()->to(route('patients.index'));
     }
 
