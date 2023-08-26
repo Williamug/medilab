caab71ffa52bafd811f00ab808cefcc4c061ecce
Asaba William <asabawilliamdk@yahoo.com>
2023-08-26 08:25:09 +0300

    Fix typos

--- resources/views/pages/patients/show.blade.php
+++ resources/views/pages/patients/show.blade.php
@@ -460,7 +460,19 @@
                         class="text-base font-semibold leading-6 fi-section-header-heading text-gray-950 dark:text-white">
                         Test Orders
                     </h3>
+                </div>
 
+                <div>
+                    <button wire:click="openCreateModal"
+                        class="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+
+                        <span>New test order</span>
+                    </button>
                 </div>
             </header>
 
@@ -517,10 +529,17 @@
                                                 </div>
                                             </td>
                                             <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-                                                <div
-                                                    class="inline px-2 py-1 text-sm font-semibold text-yellow-500 border border-yellow-300 rounded gap-x-2 bg-yellow-100/60 dark:bg-gray-800">
-                                                    {{ $test_order->order_status }}
-                                                </div>
+                                                @if ($test_order->order_status === 'submitted')
+                                                    <div
+                                                        class="inline px-2 py-1 text-sm font-semibold text-yellow-500 border border-yellow-300 rounded gap-x-2 bg-yellow-100/60 dark:bg-gray-800">
+                                                        {{ $test_order->order_status }}
+                                                    </div>
+                                                @elseif($test_order->order_status === 'received')
+                                                    <div
+                                                        class="inline px-2 py-1 text-sm font-semibold text-green-500 border border-green-300 rounded gap-x-2 bg-green-100/60 dark:bg-gray-800">
+                                                        {{ $test_order->order_status }}
+                                                    </div>
+                                                @endif
                                             </td>
                                             <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
                                                 <div
@@ -590,7 +609,7 @@
                 <div class="grid flex-1 gap-y-1">
                     <h3
                         class="text-base font-semibold leading-6 fi-section-header-heading text-gray-950 dark:text-white">
-                        Next of Kin info
+                        Next of Kin
                     </h3>
 
                 </div>
