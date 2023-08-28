de099baaf3d6dc59e6bd310a2df65a6f32a3f20a
Asaba William <asabawilliamdk@yahoo.com>
2023-08-28 14:07:41 +0300

    Update model

--- app/Models/TestResult.php
+++ app/Models/TestResult.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TestResult extends Model
@@ -18,5 +19,15 @@ class TestResult extends Model
         return empty($query)
             ? static::query()
             : static::where('test_identity', 'like', '%' . $query . '%');
+    }
+
+    public function test_order(): BelongsTo
+    {
+        return $this->belongsTo(TestOrder::class);
+    }
+
+    public function patient(): BelongsTo
+    {
+        return $this->belongsTo(Patient::class);
     }
 }
