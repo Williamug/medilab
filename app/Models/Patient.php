3affce085f09a3e2740a34fe9133433f72018b50
Asaba William <asabawilliamdk@yahoo.com>
2023-08-28 14:07:28 +0300

    Add relationship and eager load test results

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -16,7 +16,7 @@ class Patient extends Model
     protected $guarded = [];
 
     // eager load models
-    protected $with = ['patient_visits', 'test_orders'];
+    protected $with = ['patient_visits', 'test_orders', 'test_results'];
 
     //Accessor for Age.
     public function ageFromDob()
@@ -41,5 +41,10 @@ class Patient extends Model
     public function test_orders(): HasMany
     {
         return $this->hasMany(TestOrder::class);
+    }
+
+    public function test_results(): HasMany
+    {
+        return $this->hasMany(TestResult::class);
     }
 }
