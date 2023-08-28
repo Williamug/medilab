126ce7256a5291c20a7041cce2090ac9bcd96e2e
Asaba William <asabawilliamdk@yahoo.com>
2023-08-28 07:45:34 +0300

    Update component

--- app/Http/Livewire/Accountings/AccountingComponent.php
+++ app/Http/Livewire/Accountings/AccountingComponent.php
@@ -16,8 +16,14 @@ class AccountingComponent extends Component
 
     public function mount(): void
     {
-        $this->patients = Patient::all();
+        $this->patients = Patient::latest()->get();
+        // $this->patients = Patient::query()
+        //     ->join('test_orders', 'patients.id',  '=', 'test_orders.patient_id')
+        //     ->orderBy('test_orders.payment_status', 'desc')
+        //     ->select('patients.*')
+        //     ->get();
     }
+
     public function render()
     {
         return view('livewire.accountings.accounting-component');
