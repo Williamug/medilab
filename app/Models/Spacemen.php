089ed156b7e2a42c6cd8df3f275afadfb1a89194
Asaba William <asabawilliamdk@yahoo.com>
2023-08-28 17:14:04 +0300

    Add relationship

--- app/Models/Spacemen.php
+++ app/Models/Spacemen.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Spacemen extends Model
@@ -20,5 +21,10 @@ class Spacemen extends Model
         return empty($query)
             ? static::query()
             : static::where('spacemen', 'like', '%' . $query . '%');
+    }
+
+    public function test_results(): BelongsToMany
+    {
+        return $this->belongsToMany(TestResult::class);
     }
 }
