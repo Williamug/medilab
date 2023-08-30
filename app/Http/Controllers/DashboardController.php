2aaf4cded5dc8b8a9346a6cffb1f0daf10b89560
Asaba William <asabawilliamdk@yahoo.com>
2023-08-30 15:15:39 +0300

    Refactor: extract arrays to function

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,37 +2,69 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
-use App\Models\DataFeed;
 use App\Models\LabService;
 use App\Models\Patient;
 use App\Models\TestResult;
-use Carbon\Carbon;
+use Illuminate\Contracts\View\View;
 use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
 class DashboardController extends Controller
 {
-
-    /**
-     * Displays the dashboard screen
-     *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function index()
+    public function index(): View
     {
-        // $this->authorize('view  dashboard module');
-        $patient_chart_options = [
+        // initialize charts
+        $patients_chart     = new LaravelChart($this->patientChart());
+        $total_income_chart = new LaravelChart($this->incomeChart());
+
+        $patients     = Patient::count();
+        $lab_services = LabService::count();
+        $test_results = TestResult::query()
+            ->whereNotNull('result_option_id')
+            ->count();
+        $total_income = TestResult::query()
+            ->where('payment_status', 'paid')
+            ->get();
+
+        $greetings = $this->greetings();
+
+        return view('pages/dashboard/dashboard', compact('patients', 'lab_services', 'test_results', 'test_results', 'total_income', 'patients_chart', 'total_income_chart', 'greetings'));
+    }
+
+    private function greetings(): string
+    {
+        // This sets the $time variable to the current hour in the 24 hour clock format 
+        $time = date("H");
+
+        // match expresion 
+        return match (true) {
+            $time < "12" => 'Good morning',
+            $time >= "12" &&
+                $time < "17" => "Good afternoon",
+            $time >= "17" &&
+                $time < "19"  => "Good evening",
+            $time >= "19" => "Good night"
+        };
+    }
+
+    // chart to display total number of patients per month
+    private function patientChart(): array
+    {
+        return [
             'chart_title'     => 'Patient by months',
             'report_type'     => 'group_by_date',
             'model'           => Patient::class,
             'group_by_field'  => 'created_at',
             'group_by_period' => 'month',
             'chart_type'      => 'bar',
-            'chart_height'          => '600px'
+            'chart_color'        => '0,0,255',
+            'chart_height'    => '600px'
         ];
+    }
 
-        $total_income_chart_options = [
+    // chart to display income per month
+    private function incomeChart(): array
+    {
+        return [
             'chart_title'        => 'Income by months',
             'report_type'        => 'group_by_date',
             'model'              => TestResult::class,
@@ -44,37 +76,5 @@ class DashboardController extends Controller
             'aggregate_function' => 'sum',
             'chart_height'       => '600px',
         ];
-        $patients_chart     = new LaravelChart($patient_chart_options);
-        $total_income_chart = new LaravelChart($total_income_chart_options);
-
-        $patients     = Patient::count();
-        $lab_services = LabService::count();
-        $test_results = TestResult::whereNotNull('result_option_id')->count();
-        $total_income = TestResult::where('payment_status', 'paid')->get();
-
-        $greetings = $this->greetings();
-
-        return view('pages/dashboard/dashboard', compact('patients', 'lab_services', 'test_results', 'test_results', 'total_income', 'patients_chart', 'total_income_chart', 'greetings'));
-    }
-
-    private function greetings(): string
-    {
-        $greetings = "";
-
-        // This sets the $time variable to the current hour in the 24 hour clock format 
-        $time = date("H");
-
-        // Set the $timezone variable to become the current timezone 
-        $timezone = date("e");
-
-        // match expresion 
-        return match (true) {
-            $time < "12"  => 'Good morning',
-            $time >= "12" &&
-                $time < "17" => "Good afternoon",
-            $time >= "17" &&
-                $time < "19"  => "Good evening",
-            $time >= "19" => "Good night"
-        };
     }
 }
