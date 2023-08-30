b79cd7c97db718fbc4a6aa3e185a5a13b6ff6c4b
Asaba William <asabawilliamdk@yahoo.com>
2023-08-30 10:45:22 +0300

    Add charts and stats

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use App\Models\DataFeed;
+use App\Models\LabService;
+use App\Models\Patient;
+use App\Models\TestResult;
 use Carbon\Carbon;
+use LaravelDaily\LaravelCharts\Classes\LaravelChart;
 
 class DashboardController extends Controller
 {
@@ -18,6 +22,36 @@ class DashboardController extends Controller
     public function index()
     {
         // $this->authorize('view  dashboard module');
-        return view('pages/dashboard/dashboard');
+        $patients     = Patient::count();
+        $lab_services = LabService::count();
+        $test_results = TestResult::whereNotNull('result_option_id')->count();
+        $total_income = TestResult::where('payment_status', 'paid')->get();
+
+        $patient_chart_options = [
+            'chart_title'     => 'Patient by months',
+            'report_type'     => 'group_by_date',
+            'model'           => Patient::class,
+            'group_by_field'  => 'created_at',
+            'group_by_period' => 'month',
+            'chart_type'      => 'bar',
+            'chart_height'          => '600px'
+        ];
+
+        $total_income_chart_options = [
+            'chart_title'        => 'Income by months',
+            'report_type'        => 'group_by_date',
+            'model'              => TestResult::class,
+            'group_by_field'     => 'created_at',
+            'group_by_period'    => 'month',
+            'chart_type'         => 'line',
+            'chart_color'        => '0,128,0',
+            'aggregate_field'    => 'lab_service_price',
+            'aggregate_function' => 'sum',
+            'chart_height'       => '600px',
+        ];
+        $patients_chart     = new LaravelChart($patient_chart_options);
+        $total_income_chart = new LaravelChart($total_income_chart_options);
+
+        return view('pages/dashboard/dashboard', compact('patients', 'lab_services', 'test_results', 'test_results', 'total_income', 'patients_chart', 'total_income_chart'));
     }
 }
