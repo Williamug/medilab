dcc4082929caf46a43ec4ca711471dc35f41c91a
Asaba William <asabawilliamdk@yahoo.com>
2023-09-01 10:25:04 +0300

    Update: Add `test_results` relationship and update values in the fillable property

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,16 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payment extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['patient_id', 'amount_to_pay', 'balance'];
+    protected $fillable = ['patient_id', 'payment_amount', 'payment_balance', 'payment_status'];
 
+    // fetch the patient that has this payment
     public function patient(): BelongsTo
     {
         return $this->belongsTo(Patient::class);
+    }
+
+    public function test_results(): BelongsToMany
+    {
+        return $this->belongsToMany(TestResult::class);
     }
 }
