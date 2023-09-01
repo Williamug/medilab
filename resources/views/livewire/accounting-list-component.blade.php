5031d6538b691a997bc4319052d89f7510b21151
Asaba William <asabawilliamdk@yahoo.com>
2023-09-01 10:26:09 +0300

    Remove permission

--- resources/views/livewire/accounting-list-component.blade.php
+++ resources/views/livewire/accounting-list-component.blade.php
@@ -33,12 +33,10 @@
                                 Price
                             </th>
 
-                            @can('view accounting')
-                                <th
-                                    class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
-                                    Actions
-                                </th>
-                            @endcan
+                            <th
+                                class="px-5 py-3 text-xs font-semibold tracking-wider text-left text-gray-600 uppercase bg-gray-100 border-b-2 border-gray-200">
+                                Actions
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
