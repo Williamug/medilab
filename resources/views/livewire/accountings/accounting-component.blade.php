a63589366308c8cc2f82db6abb51f2dddf6d1268
Asaba William <asabawilliamdk@yahoo.com>
2023-09-02 20:30:00 +0300

    Clean up accounting component

--- resources/views/livewire/accountings/accounting-component.blade.php
+++ resources/views/livewire/accountings/accounting-component.blade.php
@@ -1,33 +1,6 @@
 <div class="mt-6">
-    {{-- @if ($patient->test_orders->isNotEmpty()) --}}
-    @foreach ($patients as $patient)
-        <div class="flex mt-12">
-            <div class="flex-1">
-                <div class="flex ml-4 text-lg font-semibold uppercase">
-                    <div class="mr-3">
-                        Patient Name:
-                    </div>
-                    <div>
-                        {{ $patient->full_name }}
-                    </div>
-                </div>
-            </div>
-
-            <div class="flex">
-                <button wire:click="openCreateNewVisitModal"
-                    class="flex items-center justify-center w-1/2 px-5 py-2 ml-3 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-4 h-4 bi bi-arrow-right-circle"
-                        viewBox="0 0 16 16">
-                        <path fill-rule="evenodd"
-                            d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
-                    </svg>
-
-                    <span>Payment Details</span>
-                </button>
-            </div>
-        </div>
-
-        <div class="flex flex-col mt-2">
+    <div>
+        <div class="flex flex-col mt-6">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                     <div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
@@ -36,65 +9,85 @@
                                 <tr>
                                     <th scope="col"
                                         class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Order Number
-                                    </th>
-
-                                    <th scope="col"
-                                        class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Lab Service
+                                        {{-- <a wire:click.prevent="sortBy('spacemen')" role="button" href="#"
+                                            class="flex"> --}}
+                                        Patient ID
+                                        {{-- @include('partials.sort_icons', [
+                                                'field' => 'spacemen',
+                                            ])
+                                        </a> --}}
                                     </th>
 
                                     <th scope="col"
                                         class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Payment Status
+                                        {{-- <a wire:click.prevent="sortBy('created_at')" role="button" href="#"
+                                            class="flex"> --}}
+                                        Full Name
+                                        {{-- @include('partials.sort_icons', ['field' => 'created_at'])
+                                        </a> --}}
                                     </th>
 
                                     <th scope="col"
-                                        class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                                        Price (UGX.)
+                                        class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        {{-- <a wire:click.prevent="sortBy('created_at')" role="button" href="#"
+                                            class="flex"> --}}
+                                        Order ID
+                                        {{-- @include('partials.sort_icons', ['field' => 'created_at'])
+                                        </a> --}}
+                                    </th>
+
+                                    <th scope="col"
+                                        class="px-12  py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                        {{-- <a wire:click.prevent="sortBy('created_at')" role="button" href="#"
+                                            class="flex"> --}}
+                                        Total Amount (UGX.)
+                                        {{-- @include('partials.sort_icons', ['field' => 'created_at'])
+                                        </a> --}}
+                                    </th>
+
+                                    <th scope="col" class="relative py-3.5 px-4">
+                                        <span class="sr-only">Action</span>
                                     </th>
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
-                                @foreach ($patient->test_results as $test_result)
+                                @foreach ($patients as $patient)
                                     <tr>
                                         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
                                             <div>
-                                                {{ $test_result->order_number }}
+                                                {{ $patient->patient_number }}
                                             </div>
                                         </td>
 
                                         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
                                             <div>
-                                                {{ $test_result->lab_service->service_name }}
+                                                {{ $patient->full_name }}
                                             </div>
-                                        </td>
-
-                                        <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
-                                            @if ($test_result->payment_status == 'unpaid')
-                                                <div
-                                                    class="inline px-1 text-xs font-normal text-red-500 border border-red-300 rounded gap-x-2 bg-red-100/60 dark:bg-gray-800">
-                                                    {{ $test_result->payment_status }}
-                                                </div>
-                                            @elseif($test_result->payment_status == 'paid')
-                                                <div
-                                                    class="inline px-1 text-xs font-normal text-green-500 border border-green-300 rounded gap-x-2 bg-green-100/60 dark:bg-gray-800">
-                                                    {{ $test_result->payment_status }}
-                                                </div>
-                                            @endif
                                         </td>
 
                                         <td class="px-4 py-4 text-sm font-medium whitespace-nowrap">
                                             <div>
-                                                @money($test_result->lab_service_price)
+                                                <ul>
+                                                    @foreach ($patient->test_results as $test_result)
+                                                        <li class="list-disc border-b">
+                                                            {{ $test_result->order_number }}
+                                                        </li>
+                                                    @endforeach
+                                                </ul>
                                             </div>
                                         </td>
 
-                                        {{-- <td class="px-6 py-3 text-center">
+                                        <td class="px-12 py-4 text-xl font-medium whitespace-nowrap">
+                                            <div>
+                                                @money($patient->test_results->sum('lab_service_price'))
+                                            </div>
+                                        </td>
+
+
+                                        <td class="px-6 py-3 text-center">
                                             <div class="flex justify-center item-center">
-                                                <!-- view-->
-                                                <a href="{{ route('patients.show', $patient) }}"
-                                                    class="flex px-2 mr-2 text-sm text-white bg-green-500 rounded hover:bg-green-700 focus:outline-none focus:shadow-outline">
+                                                <a href="{{ route('accountings.show', $patient) }}"
+                                                    class="flex items-center justify-center w-1/2 px-5 py-2 mr-4 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600">
                                                     <svg class="w-4" xmlns="http://www.w3.org/2000/svg"
                                                         fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -103,87 +96,21 @@
                                                             stroke-width="2"
                                                             d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
                                                     </svg>
-                                                    <span class="pt-1 ml-1 text-xs">View</span>
+                                                    <span class="pt-1 ml-1 text-xs">View Details</span>
                                                 </a>
-
-                                                <!-- edit -->
-                                                <a wire:click.prevent="openEditModal({{ $patient->id }})"
-                                                    href="#"
-                                                    class="flex px-2 mr-2 text-sm text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">
-                                                    <svg class="w-4" xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                                    </svg>
-                                                    <span class="pt-1 ml-1 text-xs">Edit</span>
-                                                </a>
-
-
-                                                <!-- delete -->
-                                                <button wire:click.prevent="openDeleteModal({{ $patient->id }})"
-                                                    type="button"
-                                                    class="flex px-2 text-sm text-white bg-red-500 rounded hover:bg-red-700 focus:outline-none focus:shadow-outline">
-                                                    <svg class="w-4 pt-1" xmlns="http://www.w3.org/2000/svg"
-                                                        fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                                        <path stroke-linecap="round" stroke-linejoin="round"
-                                                            stroke-width="2"
-                                                            d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                                    </svg>
-                                                    <span class="pt-1 ml-1 text-xs">Delete</span>
-                                                </button>
                                             </div>
-                                        </td> --}}
+                                        </td>
                                     </tr>
                                 @endforeach
-                                <tr>
-                                    <td class="px-4 py-4 text-sm font-medium whitespace-nowrap"></td>
-                                    <td class="px-4 py-4 text-sm font-medium whitespace-nowrap"></td>
-                                    <td class="px-4 py-4 text-sm font-bold uppercase whitespace-nowrap">
-                                        Total</td>
-                                    <td class="py-4 text-sm font-medium uppercase whitespace-nowrap">
-                                        <span class="text-xl">
-                                            UGX.
-                                        </span>
-                                        <span class="text-2xl underline decoration-double">
-                                            @money($patient->test_results->sum('lab_service_price'))
-                                        </span>
-                                    </td>
-                                </tr>
-
-                            </tbody>
-                        </table>
-                        <table class="min-w-full divide-y">
-                            <thead></thead>
-                            <tbody>
-                                <tr>
-                                    <td class="px-12 py-4 text-sm font-medium whitespace-nowrap"></td>
-                                    <td class="px-12 py-4 text-sm font-medium whitespace-nowrap"></td>
-                                    <td class="px-12 py-4 text-sm font-medium whitespace-nowrap"></td>
-                                    <td class="py-2 text-sm font-medium uppercase whitespace-nowrap">
-                                        <a href="#" wire:click.prevent="openPayBillModal({{ $patient->id }})"
-                                            class="flex items-center justify-center w-1/2 px-5 py-2 mr-4 text-sm tracking-wide text-white transition-colors duration-200 bg-green-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-green-600 dark:hover:bg-green-500 dark:bg-green-600">
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                                                class="w-4 h-4 bi bi-cash-stack" viewBox="0 0 16 16">
-                                                <path
-                                                    d="M1 3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1H1zm7 8a2 2 0 1 0 0-4 2 2 0 0 0 0 4z" />
-                                                <path
-                                                    d="M0 5a1 1 0 0 1 1-1h14a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V5zm3 0a2 2 0 0 1-2 2v4a2 2 0 0 1 2 2h10a2 2 0 0 1 2-2V7a2 2 0 0 1-2-2H3z" />
-                                            </svg>
-
-                                            <span>Pay Bill</span>
-                                        </a>
-                                    </td>
-                                    </td>
-                                </tr>
                             </tbody>
                         </table>
                     </div>
                 </div>
             </div>
         </div>
-    @endforeach
-    {{-- @endif --}}
 
-    @include('partials.pay-bill-modal');
+        {{-- <div class="mt-6 sm:flex sm:items-center sm:justify-between ">
+            {{ $patients->links() }}
+        </div> --}}
+    </div>
 </div>
