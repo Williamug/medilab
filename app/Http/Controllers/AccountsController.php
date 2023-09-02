6e1dd5b146cc55486c24089ab851e698be994b1b
Asaba William <asabawilliamdk@yahoo.com>
2023-09-02 20:30:22 +0300

    Update: Add a show method

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Patient;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -11,5 +12,10 @@ class AccountsController extends Controller
     public function index(): View
     {
         return view('pages.accountings.index');
+    }
+
+    public function show(Patient $patient): View
+    {
+        return view('pages.accountings.show', compact('patient'));
     }
 }
