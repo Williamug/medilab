eb18130111eea2d1dd9f6199765ab28f11a7ad5b
Asaba William <asabawilliamdk@yahoo.com>
2023-09-04 14:32:52 +0300

    Create assets

---
+++ resources/views/components/app/print-header.blade.php
@@ -0,0 +1,22 @@
+<div>
+    <div class="flex items-center justify-center">
+        <img src="{{ asset('logo/logo.png') }}" class="object-scale-down w-16 h-16 mt-12">
+    </div>
+    <div class="text-2xl font-bold text-center uppercase">
+        Keisha Medical Lab
+    </div>
+    <div class="text-xs text-center capitalize">
+        <div>
+            <span>Location: </span>
+            <span>Lungujja, Lubaga division </span>
+        </div>
+        <div>
+            <span>Tel: </span>
+            <span>+256 787 787 777 | +256 787 787 777</span>
+        </div>
+        <div>
+            <span>Email: </span>
+            <span class="lowercase">info@keishamedicallab.xyz</span>
+        </div>
+    </div>
+</div>
