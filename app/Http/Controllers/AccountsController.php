9cfd5309d24b19ba946aa47479ad0cbed584492b
Asaba William <asabawilliamdk@yahoo.com>
2023-09-04 14:28:11 +0300

    Add method to print receipt

--- app/Http/Controllers/AccountsController.php
+++ app/Http/Controllers/AccountsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Patient;
+use App\Models\Payment;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -17,5 +18,11 @@ class AccountsController extends Controller
     public function show(Patient $patient): View
     {
         return view('pages.accountings.show', compact('patient'));
+    }
+
+    public function printReceipt(Patient $patient): View
+    {
+        $payment = Payment::where('patient_id', $patient->id)->latest()->limit(1)->first();
+        return view('pages.accountings.print-receipt', compact('patient', 'payment'));
     }
 }
