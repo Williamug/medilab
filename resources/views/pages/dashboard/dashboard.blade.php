feb6cc83fcd2fc5a854eafc580648492611e41e5
Asaba William <asabawilliamdk@yahoo.com>
2023-09-05 15:36:20 +0300

    wip

--- resources/views/pages/dashboard/dashboard.blade.php
+++ resources/views/pages/dashboard/dashboard.blade.php
@@ -151,6 +151,98 @@
 
         </div>
 
+        <!-- Cards -->
+        <div class="grid gap-6 mb-4 md:grid-cols-2 xl:grid-cols-4">
+            <!-- Card -->
+            <x-app.dashboard-card>
+                <x-slot:svg>
+                    <div
+                        class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path
+                                d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z">
+                            </path>
+                        </svg>
+                    </div>
+                </x-slot:svg>
+
+                <x-slot:content>
+                    Patient
+                </x-slot:content>
+
+                <x-slot:counter>
+                    {{ $patients }}
+                </x-slot:counter>
+            </x-app.dashboard-card>
+
+            <!-- Card -->
+            <x-app.dashboard-card>
+                <x-slot:svg>
+                    <div
+                        class="p-3 mr-4 text-green-500 bg-green-100 rounded-full dark:text-green-100 dark:bg-green-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-5 h-5 bi bi-inboxes"
+                            viewBox="0 0 16 16">
+                            <path
+                                d="M4.98 1a.5.5 0 0 0-.39.188L1.54 5H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0A.5.5 0 0 1 10 5h4.46l-3.05-3.812A.5.5 0 0 0 11.02 1H4.98zm9.954 5H10.45a2.5 2.5 0 0 1-4.9 0H1.066l.32 2.562A.5.5 0 0 0 1.884 9h12.234a.5.5 0 0 0 .496-.438L14.933 6zM3.809.563A1.5 1.5 0 0 1 4.981 0h6.038a1.5 1.5 0 0 1 1.172.563l3.7 4.625a.5.5 0 0 1 .105.374l-.39 3.124A1.5 1.5 0 0 1 14.117 10H1.883A1.5 1.5 0 0 1 .394 8.686l-.39-3.124a.5.5 0 0 1 .106-.374L3.81.563zM.125 11.17A.5.5 0 0 1 .5 11H6a.5.5 0 0 1 .5.5 1.5 1.5 0 0 0 3 0 .5.5 0 0 1 .5-.5h5.5a.5.5 0 0 1 .496.562l-.39 3.124A1.5 1.5 0 0 1 14.117 16H1.883a1.5 1.5 0 0 1-1.489-1.314l-.39-3.124a.5.5 0 0 1 .121-.393zm.941.83.32 2.562a.5.5 0 0 0 .497.438h12.234a.5.5 0 0 0 .496-.438l.32-2.562H10.45a2.5 2.5 0 0 1-4.9 0H1.066z" />
+                        </svg>
+                    </div>
+                </x-slot:svg>
+
+                <x-slot:content>
+                    Lab Services
+                </x-slot:content>
+
+                <x-slot:counter>
+                    {{ $lab_services }}
+                </x-slot:counter>
+            </x-app.dashboard-card>
+
+
+            <!-- Card -->
+            <x-app.dashboard-card>
+                <x-slot:svg>
+                    <div class="p-3 mr-4 text-teal-500 bg-teal-100 rounded-full dark:text-teal-100 dark:bg-teal-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                            class="w-6 h-6 bi bi-prescription2" viewBox="0 0 16 16">
+                            <path d="M7 6h2v2h2v2H9v2H7v-2H5V8h2V6Z" />
+                            <path
+                                d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v10.5a1.5 1.5 0 0 1-1.5 1.5h-7A1.5 1.5 0 0 1 3 14.5V4a1 1 0 0 1-1-1V1Zm2 3v10.5a.5.5 0 0 0 .5.5h7a.5.5 0 0 0 .5-.5V4H4ZM3 3h10V1H3v2Z" />
+                        </svg>
+                    </div>
+                </x-slot:svg>
+
+                <x-slot:content>
+                    Total Tests Done
+                </x-slot:content>
+
+                <x-slot:counter>
+                    {{ $test_results }}
+                </x-slot:counter>
+            </x-app.dashboard-card>
+
+            <!-- Card -->
+            <x-app.dashboard-card>
+                <x-slot:svg>
+                    <div class="p-3 mr-4 text-blue-500 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-500">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd"
+                                d="M4 4a2 2 0 00-2 2v4a2 2 0 002 2V6h10a2 2 0 00-2-2H4zm2 6a2 2 0 012-2h8a2 2 0 012 2v4a2 2 0 01-2 2H8a2 2 0 01-2-2v-4zm6 4a2 2 0 100-4 2 2 0 000 4z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                </x-slot:svg>
+
+                <x-slot:content>
+                    Total Income
+                </x-slot:content>
+
+                <x-slot:counter>
+                    {{ $total_income->sum('payment_amount') }}
+                </x-slot:counter>
+            </x-app.dashboard-card>
+
+        </div>
+
         <!-- Dashboard actions -->
         <div class="justify-center mt-8 md:flex min-w-max">
             <div class="grid gap-6 mb-8 md:grid-cols-3">
